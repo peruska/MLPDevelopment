c539bceefd1e009eea9a5a23a5a13c273e6c4b2e
Petar Lukic <peruskatestira@gmail.com>
2018-07-03 12:57:49 +0200

    NewBranching

--- app/src/main/java/hughes/alex/marinerlicenceprep/activities/EditSubscriptionProfile.kt
+++ app/src/main/java/hughes/alex/marinerlicenceprep/activities/EditSubscriptionProfile.kt
@@ -1,23 +1,16 @@
 package hughes.alex.marinerlicenceprep.activities
 
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.app.ActionBar
-import com.squareup.picasso.Picasso
-import hughes.alex.marinerlicenceprep.MyApp
-import hughes.alex.marinerlicenceprep.MyApp.Companion.BASE_URL
+import android.support.v7.app.AppCompatActivity
 import hughes.alex.marinerlicenceprep.R
 import kotlinx.android.synthetic.main.activity_edit_subscription_profile.*
-import kotlinx.android.synthetic.main.custom_action_bar_edit_subscription.*
 
 class EditSubscriptionProfile : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_subscription_profile)
-        supportActionBar!!.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
-        supportActionBar!!.setCustomView(R.layout.custom_action_bar_edit_subscription)
-        custom_back_click.setOnClickListener { v-> finish()}
+        cancel.setOnClickListener { finish() }
     }
 }
--- app/src/main/java/hughes/alex/marinerlicenceprep/fragments/SearchFragment.kt
+++ app/src/main/java/hughes/alex/marinerlicenceprep/fragments/SearchFragment.kt
@@ -2,15 +2,22 @@ package hughes.alex.marinerlicenceprep.fragments
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import hughes.alex.marinerlicenceprep.R
+import hughes.alex.marinerlicenceprep.database.Queries
+import hughes.alex.marinerlicenceprep.uiAdapters.SearchAdapter
+import kotlinx.android.synthetic.main.search_fragment.view.*
 
 
 class SearchFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.search_fragment, container, false)
+        val view = inflater.inflate(R.layout.search_fragment, container, false)
+        view.searchRecyclerView.layoutManager = LinearLayoutManager(context)
+        view.searchRecyclerView.adapter = SearchAdapter(Queries.getQuestions(context!!, "1", "-1", "2", "-1", "-1"), context!!)
+        return view
     }
 }
